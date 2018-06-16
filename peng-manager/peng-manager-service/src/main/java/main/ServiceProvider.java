42e78503453e49b5b60d03559a64e5253214e34a
yapengren <404210666@qq.com>
2018-06-16 16:01:44 +0800

    使用FastDFSClient上传文件测试类

---
+++ peng-manager/peng-manager-service/src/main/java/main/ServiceProvider.java
@@ -0,0 +1,22 @@
+package main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * peng-manager 启动服务类
+ *
+ * @author renyapeng
+ * @date 2018/06/14
+ */
+public class ServiceProvider {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceProvider.class);
+
+    public static void main(String[] args) {
+        LOGGER.info(">>>>> peng-manager-service 正在启动 <<<<<");
+        com.alibaba.dubbo.container.Main.main(null);
+        LOGGER.info(">>>>> peng-manager-service 启动完成 <<<<<");
+    }
+}
